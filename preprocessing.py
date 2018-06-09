5f6be6c2d80102a8cc2637f15f4edb01fa4ca637
Saroj Mali <3282931269@qq.com>
2018-06-09 17:27:05 +0545

    minor changes

--- preprocessing.py
+++ preprocessing.py
@@ -4,30 +4,31 @@ import os
 import glob
 import csv
 #import shutil
+from fuk import SMA, EMA, MACD, RSI, movingaverage
 
 
 def cleancsv(source):
     try:
-        data = pd.read_csv(source, parse_dates=True)
+        df = pd.read_csv(source, parse_dates=True)
         print('ok')
     except (FileNotFoundError, IOError):
         print('Wrong file or file path.')
         return
-    if data.empty:
-        print('data empty')
+    if df.empty:
+        print('df empty')
         return
 
     # shutil.copyfile(source, destination + source)
     # print('ok')
 
-    data = data.drop_duplicates(subset='Date', keep='first')
-    data = data.set_index(pd.DatetimeIndex(data['Date']))
+    df = df.drop_duplicates(subset='Date', keep='first')
+    df = df.set_index(pd.DatetimeIndex(df['Date']))
 
-    #data.set_index('Date', drop=False, inplace=True)
+    #df.set_index('Date', drop=False, inplace=True)
 
-    idx = pd.date_range(data.index.min(), data.index.max())
+    idx = pd.date_range(df.index.min(), df.index.max())
 
-    indexed_data = data.reindex(index=idx, fill_value=np.nan)
+    indexed_data = df.reindex(index=idx, fill_value=np.nan)
 
     indexed_data = indexed_data.replace('0', np.nan)
     indexed_data = indexed_data.fillna(method='ffill')
@@ -44,20 +45,20 @@ def cleancsv(source):
 
 def calcopening(source):
     try:
-        data = pd.read_csv(source, index_col=0, parse_dates=True)
+        df = pd.read_csv(source, index_col=0, parse_dates=True)
     except (FileNotFoundError, IOError):
         print('Wrong file or file path.')
         return
-    if data.empty:
+    if df.empty:
         return
 
-    data['Opening Price'] = data['Closing Price'].shift(1)
+    df['Opening Price'] = df['Closing Price'].shift(1)
     # The Opening Price must be adjusted so that it is smaller than Maximum Price
     # and larger than Minimum Price
-    data['Maximum Price'] = data[['Opening Price', 'Maximum Price', 'Minimum Price', 'Closing Price']].max(axis=1)
-    data['Minimum Price'] = data[['Opening Price', 'Maximum Price', 'Minimum Price', 'Closing Price']].min(axis=1)
-    data.set_value(data.index[0], 'Opening Price', data.get_value(data.index[0], 'Closing Price'))
-    data.to_csv(source, index=True)
+    df['Maximum Price'] = df[['Opening Price', 'Maximum Price', 'Minimum Price', 'Closing Price']].max(axis=1)
+    df['Minimum Price'] = df[['Opening Price', 'Maximum Price', 'Minimum Price', 'Closing Price']].min(axis=1)
+    df.set_value(df.index[0], 'Opening Price', df.get_value(df.index[0], 'Closing Price'))
+    df.to_csv(source, index=True)
     print('complete')
 
 
@@ -70,6 +71,24 @@ def cleanall(source, destination):
         cleancsv(filename)
 
 
+def addtechnicalindicators(source):
+    try:
+        df = pd.read_csv(source, index_col=0, parse_dates=True)
+    except (FileNotFoundError, IOError):
+        print('Wrong file or file path.')
+        return
+    if df.empty:
+        return
+    df = SMA(df)
+    df = EMA(df)
+    df = MACD(df)
+    df = RSI(df)
+    df = movingaverage(df)
+
+    df = df.round(5)
+    df.to_csv(source, index=True)
+    print('done')
+
 def applyfunc(func, source, *args, **kwargs):
     # os.chdir(source)
     for file in glob.glob("*.csv"):
@@ -81,3 +100,5 @@ if __name__ == "__main__":
     cleanall('./data/', './data/')
 
     applyfunc(calcopening, './data/')
+
+    applyfunc(addtechnicalindicators, './data/')
